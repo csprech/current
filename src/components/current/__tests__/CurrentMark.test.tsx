a0d26585bd490b1e15e2d07f25ed29b2b779cda5
Craig Sprecher <128510977+csprech@users.noreply.github.com>
2026-07-15 21:43:29 -0700

    test: define Current brand asset contract

--- src/components/current/__tests__/CurrentMark.test.tsx
+++ src/components/current/__tests__/CurrentMark.test.tsx
@@ -9,6 +9,16 @@ describe("CurrentMark", () => {
     expect(screen.getByRole("img", { name: "Current" })).toBeInTheDocument();
   });
 
+  it("references the approved Current brand asset family", () => {
+    const { container } = render(<CurrentMark showWordmark wordmarkTone="color" />);
+
+    expect(container.querySelector('img[src="/brand/current-icon-color.svg"]')).toBeInTheDocument();
+    expect(container.querySelector('img[src="/brand/current-icon-white.svg"]')).toBeInTheDocument();
+    expect(container.querySelector('img[src="/brand/current-logo-black.svg"]')).toBeInTheDocument();
+    expect(container.querySelector('img[src="/brand/current-logo-color.svg"]')).toBeInTheDocument();
+    expect(container.querySelector('img[src="/brand/current-logo-white.svg"]')).toBeInTheDocument();
+  });
+
   it("contributes one accessible name when composed in a button", () => {
     render(
       <button type="button">
